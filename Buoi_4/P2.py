799598312ab16366b7cf13e0c665977387c3d9cc
Hieu Day <hieukaj@gmail.com>
2024-11-09 21:11:19 +0700

    BTVN_buoi4

--- Buoi_4/P2.py
+++ Buoi_4/P2.py
@@ -1,1 +1,21 @@
-n = int(input("Nhập số phần tử n = "))a = [input(f'Nhập a[{i}] = ') for i in range(n)]m = int(input("Nhập số phần tử m = "))b = [input(f'Nhập b[{i}] = ') for i in range(m)]c = []i, j = 0, 0while i < n and j < m:    c.append(a[i])    c.append(b[j])    i += 1    j += 1while i < n:    c.append(a[i])    i += 1while j < m:    c.append(b[j])    j += 1print("Danh sách thứ 3:", c)
+n = int(input("Nhập số phần tử n = "))
+a = [input(f'Nhập a[{i}] = ') for i in range(n)]
+m = int(input("Nhập số phần tử m = "))
+b = [input(f'Nhập b[{i}] = ') for i in range(m)]
+
+c = []
+i, j = 0, 0
+
+while i < n and j < m:
+    c.append(a[i])
+    c.append(b[j])
+    i += 1
+    j += 1
+while i < n:
+    c.append(a[i])
+    i += 1
+while j < m:
+    c.append(b[j])
+    j += 1
+
+print(c)
